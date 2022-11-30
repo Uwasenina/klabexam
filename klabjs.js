fa9acc70cd158953aa3f9972f553ab062c598665
uwasenina <uwasenina63@gmail.com>
2022-11-30 08:42:24 -0800

    Klab exam

---
+++ klabjs.js
@@ -0,0 +1,53 @@
+const item = [
+    { name: "Bike", price: 100 },
+    { name: "TV", price: 200 },
+    { name: "Album", price: 10 },
+    { name: "Book", price: 5 },
+    { name: "Phone", price: 500 },
+    { name: "Computer", price: 1000 },
+  ];
+  
+  let cost_item = [];
+  
+  
+  
+  item.forEach((Element) => {
+    cost_item .push(Element.price);
+  });
+  cost_item .sort((a, b) => a - b);
+  
+  //   Object of cheapest product
+  
+  const cheapProduct = item.filter((e) => {
+    return e.price === cost_item [0];
+  });
+  
+  console.log(cheapProduct);
+  //   Object of expensive product
+  
+  const expensiveProduct = item.filter((e) => {
+    return e.price === cost_item [cost_item .length - 1];
+  });
+  console.log(expensiveProduct);
+  const Remove10 = item.filter((e) => {
+    return e.price !== 10;
+  });
+  
+  function add(params) {
+    let sum = 0;
+    params.forEach((e) => {
+      sum += e.price;
+    });
+    return sum;
+  }
+  
+  //   3 Return the total of all prices of product
+  
+  let totalOfAllProduct = add(item);
+  
+  console.log(totalOfAllProduct);
+  //   3 Return the total of all prices of product except price ten
+  
+  let totalOfPriceOfProductNotTen = add(Remove10);
+  
+  console.log(totalOfPriceOfProductNotTen);
